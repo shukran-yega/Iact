04ce39c77f3db870d3a00f2ab354261571dda57d
shukuYega <shukran32001@gmail.com>
2025-09-30 18:25:06 +0300

    Minor fix

--- build_web/flutter_service_worker.js
+++ build_web/flutter_service_worker.js
@@ -73,7 +73,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "45c10e17e19cce78a501147e2f434272
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "e0b0ea7aabfe00c0c83c90c3fb4092af",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "953ad8ee549ffced773dd62c0266211d",
+"flutter_bootstrap.js": "e793eadb902d79f837488bf4844493ae",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
