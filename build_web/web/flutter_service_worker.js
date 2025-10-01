cb3245367d1d47f8436d25ce6b514f6f0ed76c83
shukuYega <shukran32001@gmail.com>
2025-10-01 19:09:09 +0300

    stability fix 4

--- build_web/web/flutter_service_worker.js
+++ build_web/web/flutter_service_worker.js
@@ -73,7 +73,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "45c10e17e19cce78a501147e2f434272
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "e0b0ea7aabfe00c0c83c90c3fb4092af",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "62e853d27e0d1fbc8a06957e875037c0",
+"flutter_bootstrap.js": "bc74fe77259e656bce38d486b05c4c9f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
